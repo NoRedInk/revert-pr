1f12f4ec6a6c67ded950af8856837f95ee5145ce
Stoeffel <schtoeffel@gmail.com>
2023-03-06 12:17:31 +0100

    Adding an options parser to our script

--- revert-pr.hs
+++ revert-pr.hs
@@ -1,18 +1,22 @@
 #!/usr/bin/env nix-script-haskell
 #!runtimeInputs fzf
 #!haskellPackages turtle
+{-# LANGUAGE ApplicativeDo #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 import Turtle
 import Prelude
 
+newtype Options = Options {maybePR :: Maybe Int}
+  deriving (Show)
+
+optionsParser :: Parser Options
+optionsParser = do
+  maybePR <- optional (argInt "PR_NUMBER" "which PR to revert")
+  pure Options {maybePR}
+
 main :: IO ()
 main = sh $ do
-  response <-
-    inproc
-      "fzf"
-      [ "--header",
-        "Are turtles cool?"
-      ]
-      ("yes" <|> "indeed")
-  echo ("You answered: " <> response)
+  Options {maybePR} <- options "Revert a PR" optionsParser
+  liftIO $ print maybePR
