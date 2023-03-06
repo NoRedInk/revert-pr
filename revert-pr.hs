d9f8661ed5d17c5ae270a8c2f492d22cb9d300ec
Stoeffel <schtoeffel@gmail.com>
2023-03-06 13:40:19 +0100

    Implement reverting

--- revert-pr.hs
+++ revert-pr.hs
@@ -1,5 +1,5 @@
 #!/usr/bin/env nix-script-haskell
-#!runtimeInputs fzf gitAndTools.gh
+#!runtimeInputs fzf gitAndTools.gh git
 #!haskellPackages turtle aeson utf8-string
 
 {-# LANGUAGE ApplicativeDo #-}
@@ -12,6 +12,7 @@ import qualified Data.ByteString.UTF8 as BS
 import Data.Maybe (mapMaybe)
 import qualified Data.Text as Text
 import GHC.Generics (Generic)
+import System.Exit (ExitCode (..))
 import Turtle
 import Prelude
 
@@ -30,8 +31,11 @@ main = sh $ do
     Nothing -> echo "TODO"
     Just pr -> do
       commits <- listCommits pr
-      selected <- selectCommits commits
-      liftIO $ print selected
+      toRevert <- selectCommits commits
+      withStash $ do
+        revertedCommits <- traverse revertCommit toRevert
+        addAll
+        commitRevert pr revertedCommits
       pure ()
 
 data Commit = Commit {oid :: Text, messageHeadline :: Text}
@@ -61,12 +65,102 @@ selectCommits commits = do
           map
             (\Commit {oid, messageHeadline} -> oid <> sep <> messageHeadline)
             commits
-  selected <- inproc "fzf" ["--multi"] (select commitLines)
+  selected <-
+    returnsList $
+      inproc
+        "fzf"
+        [ "--multi",
+          "--no-sort",
+          "--bind",
+          "ctrl-a:select-all",
+          "--preview",
+          "git show --color=always {+1}",
+          "--delimiter",
+          " ",
+          "--with-nth",
+          "2..",
+          "--header",
+          "Select commits to revert (ctrl-a to select all)"
+        ]
+        (select commitLines)
   let selectedOids =
         fmap (\line -> fst (Text.breakOn sep line)) $
-          Text.lines $ lineToText selected
+          Text.lines $ linesToText selected
   pure $
     filter (\Commit {oid} -> oid `elem` selectedOids) commits
+
+withStash :: Shell () -> Shell ()
+withStash go = do
+  unstaged <- hasUnstagedChanges
+  if unstaged
+    then do
+      _ <- stash
+      _ <- go
+      popStash
+    else go
+
+hasUnstagedChanges :: Shell Bool
+hasUnstagedChanges = do
+  response <- inshell "git status --porcelain" ""
+  pure (response /= "")
+
+stash :: Shell ()
+stash = do
+  echo "Stashing unstaged changes..."
+  shells "git stash -u" ""
+
+popStash :: Shell ()
+popStash = do
+  echo "Popping stash..."
+  shells "git stash pop" ""
+
+data Action = Reverted | ResolvedConflicts | SkippedMerge
+  deriving (Show)
+
+revertCommit :: Commit -> Shell (Action, Commit)
+revertCommit commit = do
+  isMerge <- isMergeCommit commit
+  if isMerge
+    then do
+      echoText ("Skipping merge commit " <> oid commit <> " " <> messageHeadline commit <> "...")
+      pure (SkippedMerge, commit)
+    else do
+      echoText ("Reverting " <> oid commit <> " " <> messageHeadline commit <> "...")
+      (exitCode, _, _) <- procStrictWithErr "git" ["revert", "-n", oid commit, "--strategy-option", "ours"] ""
+      case exitCode of
+        ExitSuccess -> pure (Reverted, commit)
+        ExitFailure _ -> do
+          _ <- shells "git mergetool" ""
+          pure (ResolvedConflicts, commit)
+
+isMergeCommit :: Commit -> Shell Bool
+isMergeCommit commit = do
+  let sha = oid commit
+  (_, response, _) <- procStrictWithErr "git" ["rev-list", "-1", "--merges", oid commit <> "~1.." <> oid commit] ""
+  pure (response /= "")
+
+addAll :: Shell ()
+addAll =
+  shells "git add --all" ""
+
+commitRevert :: Int -> [(Action, Commit)] -> Shell ()
+commitRevert pr actionCommit = do
+  let message = Text.unlines (("Revert #" <> Text.pack (show pr) <> "\n") : map renderActionCommit actionCommit)
+  echo "Committing revert..."
+  procs "git" ["commit", "--message", message] ""
+
+renderActionCommit :: (Action, Commit) -> Text
+renderActionCommit (action, commit) =
+  "* [" <> messageHeadline commit <> "](" <> oid commit <> ")  *" <> actionToText action <> "*"
+
+actionToText :: Action -> Text
+actionToText action =
+  case action of
+    Reverted -> "Reverted"
+    ResolvedConflicts -> "Resolved conflicts"
+    SkippedMerge -> "Skipped merge commit"
+
+-- HELPERS
 
 inprocJSON :: Aeson.FromJSON a => Text -> [Text] -> Shell Line -> Shell a
 inprocJSON command args stdin = do
@@ -74,3 +168,13 @@ inprocJSON command args stdin = do
   case Aeson.eitherDecodeStrict' (BS.fromString (Text.unpack (lineToText raw))) of
     Left err -> fail err
     Right parsed -> pure parsed
+
+echoText :: Text -> Shell ()
+echoText text =
+  case textToLine text of
+    Nothing -> pure ()
+    Just line -> echo line
+
+returnsList :: Shell a -> Shell [a]
+returnsList shell =
+  reduce (Fold (\acc x -> acc <> [x]) [] id) shell
